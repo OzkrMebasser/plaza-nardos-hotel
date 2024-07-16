d902622e70180930cf8889441740f188d158f04d
OzkrMebasser <oscarmorenoestudios@gmail.com>
2024-07-15 21:11:44 -0700

    Feat: adding metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,13 @@ import "./globals.css";
 export const metadata = {
   
   title: "Hotel Plaza Nardo's",
-  // description: <MetaData/>,
+  description: `Ubicado en el corazón de Playa del Carmen, 
+  a solo una cuadra de la Quinta Avenida, Plaza Nardo's es un hotel
+   recién remodelado que ofrece una experiencia única. Con su playa 
+   privada y fácil acceso a las mejores tiendas y restaurantes, disfrutarás 
+   de una estancia inigualable en el centro turístico de la ciudad. 
+   ¡Ven y descubre la comodidad y el encanto de Plaza Nardo's!
+`,
   icons: {
     icon: "/favicon.ico",
   },
