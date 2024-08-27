db40effd7113b4092e5f51aa44c24bd9e4283261
OzkrMebasser <oscarmorenoestudios@gmail.com>
2024-08-26 23:51:08 -0700

    Feat: adding translations in room stuff /habitaciones page

--- src/app/inicio/page.js
+++ src/app/inicio/page.js
@@ -1,13 +1,9 @@
-
 import Hero from "../components/Hero";
-import MetaData from "../components/MetaData";
 import Map from "../components/Map";
 import AmenitiesGrid from "../components/Cards/AmenitiesGrid";
 import HomeRoomsCards from "../components/Cards/HomeRoomsCards";
 
-
 export function generateMetadata() {
-
   return {
     title: `Hotel Plaza Nardo's | Inicio`,
     description: "",
@@ -17,13 +13,9 @@ export function generateMetadata() {
   };
 }
 const Home = () => {
-
-
   return (
     <>
-    
       <Hero />
-      <MetaData/>
       <HomeRoomsCards />
       <AmenitiesGrid />
       <Map />
