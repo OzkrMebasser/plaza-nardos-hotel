4dd05f5608fb69e257d57653a179a47603152368
OzkrMebasser <oscarmorenoestudios@gmail.com>
2024-09-04 14:44:58 -0700

    Feat: working in reservations form

--- src/app/components/ReservationForm.jsx
+++ src/app/components/ReservationForm.jsx
@@ -1,6 +1,6 @@
-"use client"
-import React, { useEffect, useState } from 'react';
-import { useRoomsAndCurrency } from '../contexts/RoomsAndCurrencyContext';
+"use client";
+import React, { useEffect, useState } from "react";
+import { useRoomsAndCurrency } from "../contexts/RoomsAndCurrencyContext";
 
 const ReservationForm = () => {
   const { roomsData, currency } = useRoomsAndCurrency();
@@ -15,8 +15,8 @@ const ReservationForm = () => {
 
   const handleRoomChange = (e) => {
     const roomType = e.target.value;
-    const room = roomsData.find(r => r.roomType === roomType);
-    
+    const room = roomsData.find((r) => r.roomType === roomType);
+
     setSelectedRoom(room);
     setNumberOfPeople(2); // Reset number of people to default
   };
@@ -25,7 +25,10 @@ const ReservationForm = () => {
     const selectedNumber = Number(e.target.value);
     setNumberOfPeople(selectedNumber);
 
-    const room = roomsData.find(r => r.roomType === selectedRoom.roomType && r.capacity === selectedNumber);
+    const room = roomsData.find(
+      (r) =>
+        r.roomType === selectedRoom.roomType && r.capacity === selectedNumber
+    );
     if (room) {
       setSelectedRoom(room);
     }
@@ -34,67 +37,74 @@ const ReservationForm = () => {
   if (!selectedRoom) return null;
 
   // Get unique room types
-  const uniqueRoomTypes = [...new Set(roomsData.map(room => room.roomType))];
+  const uniqueRoomTypes = [...new Set(roomsData.map((room) => room.roomType))];
 
   // Get available capacities for the selected room type
   const availableCapacities = roomsData
-    .filter(r => r.roomType === selectedRoom.roomType)
-    .map(r => r.capacity);
+    .filter((r) => r.roomType === selectedRoom.roomType)
+    .map((r) => r.capacity);
 
   // Calculate total price
-  const totalPrice = (selectedRoom.price[currency] + selectedRoom.fees[currency]).toFixed(2);
+  const totalPrice = (
+    selectedRoom.price[currency] + selectedRoom.fees[currency]
+  ).toFixed(2);
 
   return (
-    <form className="space-y-4 mt-[50rem]">
-      <div>
-        <label className="block">Selecciona el tipo de habitación:</label>
-        <select
-          className="border rounded p-2"
-          onChange={handleRoomChange}
-          value={selectedRoom.roomType}
+    <section className="flex w-full bg-red-600 lg:px-4  justify-center items-center">
+      <form className="flex-wrap lg:flex   bg-green-500 w-auto justify-center lg:space-x-20 items-center py-4 ">
+        <div>
+          <label className="block">Selecciona el tipo de habitación:</label>
+          <select
+            className="border rounded p-2"
+            onChange={handleRoomChange}
+            value={selectedRoom.roomType}
+          >
+            {uniqueRoomTypes.map((roomType) => (
+              <option key={roomType} value={roomType}>
+                {roomType}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div>
+          <label className="block">Número de personas:</label>
+          <select
+            className="border rounded p-2"
+            onChange={handlePeopleChange}
+            value={numberOfPeople}
+          >
+            {availableCapacities.map((capacity) => (
+              <option key={capacity} value={capacity}>
+                {capacity}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div>
+          <label className="block">Precio:</label>
+          <span>
+            {selectedRoom.price[currency]} {currency}
+          </span>
+          <label className="block">Impuestos:</label>
+          <span>
+            {selectedRoom.fees[currency]} {currency}
+          </span>
+          <label className="block">Total:</label>
+          <span>
+            {totalPrice} {currency}
+          </span>
+        </div>
+
+        <button
+          type="submit"
+          className="bg-blue-500 text-white p-2 rounded h-[3rem]"
         >
-          {uniqueRoomTypes.map(roomType => (
-            <option key={roomType} value={roomType}>
-              {roomType}
-            </option>
-          ))}
-        </select>
-      </div>
-
-      <div>
-        <label className="block">Número de personas:</label>
-        <select
-          className="border rounded p-2"
-          onChange={handlePeopleChange}
-          value={numberOfPeople}
-        >
-          {availableCapacities.map(capacity => (
-            <option key={capacity} value={capacity}>
-              {capacity}
-            </option>
-          ))}
-        </select>
-      </div>
-
-      <div>
-        <label className="block">Precio:</label>
-        <span>
-          {selectedRoom.price[currency]} {currency}
-        </span>
-        <label className="block">Impuestos:</label>
-        <span>
-          {selectedRoom.fees[currency]} {currency}
-        </span>
-        <label className="block">Total:</label>
-        <span>
-          {totalPrice} {currency}
-        </span>
-      </div>
-
-      <button type="submit" className="bg-blue-500 text-white p-2 rounded">
-        Reservar
-      </button>
-    </form>
+          Reservar
+        </button>
+      </form>
+    </section>
   );
 };
 
---
+++ src/app/reservaciones/page.js
@@ -0,0 +1,34 @@
+
+
+
+import ReservationForm from '../components/ReservationForm';
+
+
+
+
+export const metadata = {
+  
+
+  title: `Hotel Plaza Nardo's | reservaciones `,
+  description: "Ubicado en el corazón de Playa del Carmen, a solo una cuadra de la Quinta Avenida, Plaza Nardo's es un hotel recién remodelado que ofrece una experiencia única. Con su playa privada y fácil acceso a las mejores tiendas y restaurantes, disfrutarás de una estancia inigualable en el centro turístico de la ciudad. ¡Ven y descubre la comodidad y el encanto de Plaza Nardo's!",
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
+const Reservaciones = () => {
+  
+  return (
+    <>
+     <section className="h-[125px] bg-[#7c7498] shadow-lg top-0 left-0 w-full transition-all duration-300"></section>
+      <section className="mt-4 py-8">
+        {/* <Title title={translations.homeTitles.ourRooms} /> */}
+   <ReservationForm/>
+   </section>
+ 
+    </>
+  )
+}
+
+export default Reservaciones;
+
